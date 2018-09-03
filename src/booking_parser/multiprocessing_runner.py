2b150ff527696d54932f7397dabd20b8d197de07
Lev Kulik <kulik_li@groupbwt.com>
2018-09-03 17:43:18 +0300

    add validation rules

--- src/booking_parser/multiprocessing_runner.py
+++ src/booking_parser/multiprocessing_runner.py
@@ -4,11 +4,20 @@ from helpers.validator import Validator
 
 print("Enter booking-scraper commands and press 'Enter'")
 
+validator = Validator()
+
 for line in sys.stdin:
     if len(line) is not 1:
-        # TODO Convert string into array of args
-        # TODO Validate array of args
-        # TODO Add array of args to array of commands
+        line = line .strip()
+        command = line.split(" ")
+
+        # TODO Delete all. save only c_in date, c_out date and args
+        if validator.validate_python(command[0]) and validator.validate_script_name(command[1]) and validator.validate_input(checkin_date=command[2], checkout_date=command[3]):
+            print("OK")
+        else:
+            print("Not ok")
+        # TODO Validate command array
+        # TODO Add command array to array of commands
 
     else:
         break
