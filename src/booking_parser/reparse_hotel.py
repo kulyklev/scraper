97087099baa97ceaf62a31402d3e90fdacc5cd61
Lev Kulik <kulik_li@groupbwt.com>
2018-09-11 18:00:29 +0300

    change disabled argparse

--- src/booking_parser/reparse_hotel.py
+++ src/booking_parser/reparse_hotel.py
@@ -6,29 +6,30 @@ from helpers.validator import Validator
 
 
 # Setting up argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("hotel_link", help="Entry hotel link from booking.com")
-parser.add_argument("check_in_date", help="Check-in date format YYYY-MM-DD")
-parser.add_argument("--vpn", help="Using this flag cause parsing through vpn", action="store_true")
-parser.add_argument("-r", "--requests", help="The maximum number of concurrent requests that will be performed by the "
-                                             "Scrapy downloader.", type=int)
+# parser = argparse.ArgumentParser()
+# parser.add_argument("hotel_link", help="Entry hotel link from booking.com")
+# parser.add_argument("check_in_date", help="Check-in date format YYYY-MM-DD")
+# parser.add_argument("--vpn", help="Using this flag cause parsing through vpn", action="store_true")
+# parser.add_argument("-r", "--requests", help="The maximum number of concurrent requests that will be performed by the "
+#                                              "Scrapy downloader.", type=int)
+#
+# args = parser.parse_args()
+# kwargs = {
+#         'hotel_link': args.hotel_link,
+#         'check_in_date': args.check_in_date,
+#
+#         'use_vpn': args.vpn,
+#     }
+#
+#
+# settings = get_project_settings()
 
-args = parser.parse_args()
-kwargs = {
-        'hotel_link': args.hotel_link,
-        'check_in_date': args.check_in_date,
+# if args.requests is not None:
+#     settings['CONCURRENT_REQUESTS'] = args.requests
+#     process = CrawlerProcess(settings)
+# else:
+#     process = CrawlerProcess(get_project_settings())
 
-        'use_vpn': args.vpn,
-    }
-
-
-settings = get_project_settings()
-
-if args.requests is not None:
-    settings['CONCURRENT_REQUESTS'] = args.requests
-    process = CrawlerProcess(settings)
-else:
-    process = CrawlerProcess(get_project_settings())
-
-process.crawl('booking', kwargs=kwargs)
+process = CrawlerProcess(get_project_settings())
+process.crawl('booking')
 process.start()
